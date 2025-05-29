41a1c6988d02dfd28a421c45105d2b8cce2937de
lanoerwedding <lanoer1481.com>
2025-05-29 22:36:41 +0700

    up

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import { webcrypto as crypto } from "node:crypto";
+
+if (!globalThis.crypto) {
+    globalThis.crypto = crypto;
+}
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],

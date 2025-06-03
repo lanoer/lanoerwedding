0a2e651a3af9508e2595a30627ba124340dc8175
lanoerwedding <lanoer1481.com>
2025-06-03 14:31:34 +0700

    update

--- resources/views/front/layouts/inc/head.blade.php
+++ resources/views/front/layouts/inc/head.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>@yield('title')</title>
+    <title>@yield('pageTitle')</title>
     <base href="/">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">

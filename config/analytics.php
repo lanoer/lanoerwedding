9c0acba8da7d5ed9f599d8ed979937516dd16492
lanoerwedding <lanoer1481.com>
2025-06-05 10:25:13 +0700

    update SEO

--- config/analytics.php
+++ config/analytics.php
@@ -12,7 +12,7 @@ return [
      * to learn how to get this file. You can also pass the credentials as an array
      * instead of a file path.
      */
-    'service_account_credentials_json' => storage_path('app/analytics/lanoer-wedding-b03d5517dc0b.json'),
+    'service_account_credentials_json' => storage_path('app/analytics/lanoer-wedding-d656c3f115a5.json'),
 
     /*
      * The amount of minutes the Google API responses will be cached.

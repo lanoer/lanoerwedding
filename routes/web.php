06986182ca445c15d91a57f2c54842947a88ccfc
lanoerwedding <lanoer1481.com>
2025-06-11 01:08:27 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Back\AboutController;
-use App\Http\Controllers\back\AlbumController;
+use App\Http\Controllers\Back\AlbumController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Back\AuthController;
 use App\Http\Controllers\Back\CateringController;
